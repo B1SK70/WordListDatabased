3a387b4b0bd3618d34ffba067112d97aa7238dd8
B1SK70 <arnaubisqui70@gmail.com>
2022-03-02 16:11:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wordlistdatabased/MainActivity.java
@@ -0,0 +1,84 @@
+package com.example.wordlistdatabased;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    public static final int WORD_EDIT = 1;
+    public static final int WORD_ADD = -1;
+
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+
+    private WordListOpenHelper mDB;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mDB = new WordListOpenHelper(this);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+
+        mAdapter = new WordListAdapter(this, mDB);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+
+
+        // floating action
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Start empty edit activity.
+                Intent intent = new Intent(getBaseContext(), EditWordActivity.class);
+                startActivityForResult(intent, WORD_EDIT);
+            }
+        });
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == WORD_EDIT) {
+            if (resultCode == RESULT_OK) {
+                String word = data.getStringExtra(EditWordActivity.EXTRA_REPLY);
+                if (!TextUtils.isEmpty(word)) {
+                    int id = data.getIntExtra(WordListAdapter.EXTRA_ID, -99);
+                    if (id == WORD_ADD) {
+                        mDB.insert(word);
+                        mAdapter.notifyDataSetChanged();
+
+                    }else if (id >= 0) {
+                        mDB.update(id, word);
+                        mAdapter.notifyDataSetChanged();
+                    }
+
+                } else {
+                    Toast.makeText(
+                            getApplicationContext(),
+                            R.string.empty_not_saved,
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
+}
+
+
